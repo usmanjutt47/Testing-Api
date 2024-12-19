563fa8b04a1cc098eee655ef6493272c017a2b1d
Muhammad Usman <142773789+usmanjutt47@users.noreply.github.com>
2024-12-19 14:14:48 +0500

    Update userModel.tsx

--- src/models/userModel.tsx
+++ src/models/userModel.tsx
@@ -10,6 +10,10 @@ const userSchema = new Schema(
       type: String,
       unique: true,
       required: true
+    },
+    password: {
+      type: String,
+      required: true
     }
   },
   {
